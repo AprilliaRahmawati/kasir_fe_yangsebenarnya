4ce642cec00c01c8ca544ca6147f2f82001b3b17
AprilliaRahmawati <98732707+AprilliaRahmawati@users.noreply.github.com>
2023-06-16 09:10:17 +0700

    first

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    shim: false
+  },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:8000'
+    }
+  },
+})
